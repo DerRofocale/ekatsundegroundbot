a4faf17dd9dac59f4384866ff937200c2201d8ad
Дмитрий Яльчик <dmitry_yalchik@mail.ru>
2022-04-16 22:08:41 +0500

    ForTestInHeroku

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,6 +5,8 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("cancel", "Отменить действие"),
+            types.BotCommand("copyright", "Авторские права"),
+            types.BotCommand("sites", "Сайты"),
+            types.BotCommand("shedule", "Расписание"),
         ]
     )
